3e6910d70851fc983296666bd10b36d667889073
LapenokAlexey <aleks.lapenok@gmail.com>
2021-06-23 20:55:38 +0300

    fix:fixed submenu

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,9 +2,9 @@ import { Component } from 'react';
 import AddTask from './add-task/AddTask';
 import Submenu from './submenu/Submenu';
 import TaskList from './task-list/TaskList';
-import TaskLista from './task-list/TaskList';
-import TaskListd from './task-list/TaskList';
-import { BrowserRouter, Route } from 'react-router-dom';
+import TaskLista from './task-list/TaskLista';
+import TaskListd from './task-list/TaskListd';
+import { Route } from 'react-router-dom';
 
 class Main extends Component {
     render() {
@@ -12,45 +12,52 @@ class Main extends Component {
         const addTask = this.props.addTask;
         const deleteTask = this.props.deleteTask;
         return (
-            <BrowserRouter>
-                <main>
-                    <hr />
-                    <Submenu task={task} />
-                    <AddTask addTask={addTask} task={task} />
 
-                    <Route exact path='/all'>
-                        <TaskList
-                            deleteTask={deleteTask}
-                            handleDone={this.props.handleDone}
-                            handleIsImportant={this.props.handleIsImportant}
-                            task={task}
-                            stateApp={this.props.stateApp}
-                        />
-                    </Route>
+            <main>
+                <hr />
+                <Submenu task={task} />
+                <AddTask addTask={addTask} task={task} />
+                <Route exact path="/">
+                    <TaskList
+                        deleteTask={deleteTask}
+                        handleDone={this.props.handleDone}
+                        handleIsImportant={this.props.handleIsImportant}
+                        task={task}
+                        stateApp={this.props.stateApp}
+                    />
+                </Route>
+                {/* <Route path="/all">
+                    <TaskList
+                        deleteTask={deleteTask}
+                        handleDone={this.props.handleDone}
+                        handleIsImportant={this.props.handleIsImportant}
+                        task={task}
+                        stateApp={this.props.stateApp}
+                    />
+                </Route> */}
 
-                    <Route path='/active' >
-                        <TaskLista
-                            deleteTask={deleteTask}
-                            handleDone={this.props.handleDone}
-                            handleIsImportant={this.props.handleIsImportant}
-                            task={task}
-                            stateApp={this.props.stateApp}
-                        />
+                <Route path="/active" >
+                    <TaskLista
+                        deleteTask={deleteTask}
+                        handleDone={this.props.handleDone}
+                        handleIsImportant={this.props.handleIsImportant}
+                        task={task}
+                        stateApp={this.props.stateApp}
+                    />
 
-                    </Route>
+                </Route>
 
-                    <Route path='/done'>
-                        <TaskListd
-                            deleteTask={deleteTask}
-                            handleDone={this.props.handleDone}
-                            handleIsImportant={this.props.handleIsImportant}
-                            task={task}
-                            stateApp={this.props.stateApp}
-                        />
-                    </Route>
+                <Route path="/done">
+                    <TaskListd
+                        deleteTask={deleteTask}
+                        handleDone={this.props.handleDone}
+                        handleIsImportant={this.props.handleIsImportant}
+                        task={task}
+                        stateApp={this.props.stateApp}
+                    />
+                </Route>
 
-                </main>
-            </BrowserRouter >
+            </main>
         )
     }
 }
--- src/components/main/submenu/Submenu.jsx
+++ src/components/main/submenu/Submenu.jsx
@@ -3,20 +3,53 @@ import { NavLink } from 'react-router-dom';
 import './Submenu.css';
 class Submenu extends Component {
 
-    handleSubDone = (done) => {
-        this.props.task.map(item => (
-            console.log(item.done)
-        ))
+    state = {
+        activeClassName: "active"
     }
+
+    handleClass = () => {
+        this.setState(state => {
+            state.activeClassName = "";
+            return state;
+        })
+    }
+
+    handleClassAll = () => {
+        this.setState(state => {
+            state.activeClassName = "active";
+            return state;
+        })
+    }
+    componentDidMount() {
+        const localStorageRef = localStorage.getItem('stateSub');
+        if (localStorageRef) {
+            this.setState({ activeClassName: JSON.parse(localStorageRef) });
+        }
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem('stateSub', JSON.stringify(this.state.activeClassName));
+    }
+
     render() {
-        // const task = this.props.task;
-        // const index = task.map(item => item.done);
+
         return (
             <section className="submenu-container">
                 <div className="submenu-container_submenu">
-                    <NavLink to="/all" className="submenu-container_submenu_all" tabIndex="2">All</NavLink>
-                    <NavLink to="/active" className="submenu-container_submenu_active" tabIndex="3">Active</NavLink>
-                    <NavLink to="/done" className="submenu-container_submenu_done" tabIndex="4">Done</NavLink>
+                    <NavLink
+                        to="/"
+                        onClick={() => this.handleClassAll()}
+                        className="submenu-container_submenu_all"
+                        activeClassName={this.state.activeClassName}
+                        tabIndex="2"
+                    >All
+                    </NavLink>
+                    <NavLink
+                        to="/active"
+                        onClick={() => this.handleClass()}
+                        className="submenu-container_submenu_active"
+                        tabIndex="3">Active</NavLink>
+                    <NavLink to="/done" onClick={() => this.handleClass()} className="submenu-container_submenu_done" tabIndex="4">Done</NavLink>
                 </div>
             </section>
         )
